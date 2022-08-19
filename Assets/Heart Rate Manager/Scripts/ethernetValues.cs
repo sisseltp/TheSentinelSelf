f122283365c6c8cdea4b1fa57dec0d44cc7cc359
Sam Bilbow <31018239+sambilbow@users.noreply.github.com>
2022-08-19 11:06:25 +0100

    Update ethernetValues.cs
    Hide unused sensor variables, tidy inspector

--- Assets/Heart Rate Manager/Scripts/ethernetValues.cs
+++ Assets/Heart Rate Manager/Scripts/ethernetValues.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
 using UnityEngine;
@@ -6,34 +6,36 @@ using Freya;
 
 public class ethernetValues : MonoBehaviour
 {
-    [Header("Sentinel 1 Values")]
+    [Header("Sensor Values")]
     public int sentinel1Rate;
     public int sentinel1Interval;
     public float sentinel1Pulse;
-    
-    [Header("Sentinel 2 Values")]
-    public int sentinel2Rate;
-    public int sentinel2Interval;
-    public float sentinel2Pulse;
-    
-    [Header("Sentinel 3 Values")]
-    public int sentinel3Rate;
-    public int sentinel3Interval;
-    public float sentinel3Pulse;
-    
-    [Header("Sentinel 4 Values")]
-    public int sentinel4Rate;
-    public int sentinel4Interval;
-    public float sentinel4Pulse;
 
+    //[Header("Sentinel 2 Values")]
+    //public int sentinel2Rate;
+    //public int sentinel2Interval;
+    //public float sentinel2Pulse;
 
+    //[Header("Sentinel 3 Values")]
+    //public int sentinel3Rate;
+    //public int sentinel3Interval;
+    //public float sentinel3Pulse;
+
+    //[Header("Sentinel 4 Values")]
+    //public int sentinel4Rate;
+    //public int sentinel4Interval;
+    //public float sentinel4Pulse;
+
+    [Header("Kuramoto Values")]
     public float pulseGradient;
     private KuramotoAffecterAgent agent;
     private const float CIRCLE_IN_RADIAN = 2f * Mathf.PI; //2* pi
     public int bias = 3;
 
+
     private bool playing = false;
     private bool reading = false;
+    [Header("Scene Threshold Values")]
     [SerializeField]
     private float beginTimer = 2;
     [SerializeField]
@@ -65,7 +67,7 @@ public class ethernetValues : MonoBehaviour
 
         float avrgChange = Mathf.Abs(sentinel1Rate - lastAvrgRate);
 
-       
+
 
         if (avrgRate > 50 && avrgRate < 150 && avrgChange < changeGate)
         {
@@ -74,7 +76,7 @@ public class ethernetValues : MonoBehaviour
                 reading = true;
                 TimerGate = Time.time;
             }
-            else if(!playing && TimerGate + beginTimer<Time.time)
+            else if (!playing && TimerGate + beginTimer < Time.time)
             {
                 playing = true;
                 IntroControl.Begin();
@@ -106,7 +108,7 @@ public class ethernetValues : MonoBehaviour
             reading = false;
             TimerGate = Time.time;
         }
-        else if(playing && TimerGate + restartTimer < Time.time)
+        else if (playing && TimerGate + restartTimer < Time.time)
         {
             IntroControl.Restart();
             playing = false;
@@ -114,7 +116,7 @@ public class ethernetValues : MonoBehaviour
 
         lastAvrgRate = avrgRate;
     }
-    
+
     public void setSentinelAgent(KuramotoAffecterAgent thisAgent)
     {
         agent = thisAgent;
@@ -133,48 +135,48 @@ public class ethernetValues : MonoBehaviour
 
     public void setSentinel1Pulse(int value)
     {
-        sentinel1Pulse = Mathfs.Remap(0f,4095f,0f,1f,value);
+        sentinel1Pulse = Mathfs.Remap(0f, 4095f, 0f, 1f, value);
     }
-    public void setSentinel2Rate(int value)
-    {
-        sentinel2Rate = value;
-    }
+    //public void setSentinel2Rate(int value)
+    //{
+    //    sentinel2Rate = value;
+    //}
 
-    public void setSentinel2Interval(int value)
-    {
-        sentinel2Interval = value;
-    }
+    //public void setSentinel2Interval(int value)
+    //{
+    //    sentinel2Interval = value;
+    //}
 
-    public void setSentinel2Pulse(int value)
-    {
-        sentinel2Pulse = Mathfs.Remap(0f,4095f,0f,1f,value);
-    }
-    public void setSentinel3Rate(int value)
-    {
-        sentinel3Rate = value;
-    }
+    //public void setSentinel2Pulse(int value)
+    //{
+    //    sentinel2Pulse = Mathfs.Remap(0f,4095f,0f,1f,value);
+    //}
+    //public void setSentinel3Rate(int value)
+    //{
+    //    sentinel3Rate = value;
+    //}
 
-    public void setSentinel3Interval(int value)
-    {
-        sentinel3Interval = value;
-    }
+    //public void setSentinel3Interval(int value)
+    //{
+    //    sentinel3Interval = value;
+    //}
 
-    public void setSentinel3Pulse(int value)
-    {
-        sentinel3Pulse = Mathfs.Remap(0f,4095f,0f,1f,value);
-    }
+    //public void setSentinel3Pulse(int value)
+    //{
+    //    sentinel3Pulse = Mathfs.Remap(0f,4095f,0f,1f,value);
+    //}
 
-    public void setSentinel4Rate(int value) {
-        sentinel4Rate = value;
-    }
+    //public void setSentinel4Rate(int value) {
+    //    sentinel4Rate = value;
+    //}
 
-    public void setSentinel4Interval(int value) {
-        sentinel4Interval = value;
-    }
+    //public void setSentinel4Interval(int value) {
+    //    sentinel4Interval = value;
+    //}
 
-    public void setSentinel4Pulse(int value) {
-        sentinel4Pulse = Mathfs.Remap(0f,4095f,0f,1f,value);
-    }
+    //public void setSentinel4Pulse(int value) {
+    //    sentinel4Pulse = Mathfs.Remap(0f,4095f,0f,1f,value);
+    //}
 
 }
 
