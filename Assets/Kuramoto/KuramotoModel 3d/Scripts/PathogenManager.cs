6385e80529bd853accdec7e197762919f8a9fb16
george simms <simmsgeorge7@gmail.com>
2022-08-31 00:00:58 +0100

    tested version

--- Assets/Kuramoto/KuramotoModel 3d/Scripts/PathogenManager.cs
+++ Assets/Kuramoto/KuramotoModel 3d/Scripts/PathogenManager.cs
@@ -123,6 +123,7 @@ public class PathogenManager : MonoBehaviour
         Vector3 pos = transform.position + UnityEngine.Random.insideUnitSphere * spawnArea;
 
         // instantiate a new sentinel as child and at pos
+
         GameObject thisSentinel = Instantiate(sentinel, pos, Quaternion.identity, this.transform);
 
         // get its kurmto component
@@ -145,7 +146,7 @@ public class PathogenManager : MonoBehaviour
         for (int l = 0; l < duplications; l++)
         {
             // instantiate a new sentinel as child and at pos
-            GameObject thisSentinel = Instantiate(pathogen, pathogen.transform.position, Quaternion.identity, this.transform);
+            GameObject thisSentinel = Instantiate(pathogen, pathogen.transform.position, pathogen.transform.rotation, this.transform);
 
             // get its kurmto component
             KuramotoAffectedAgent kuramoto = thisSentinel.GetComponent<KuramotoAffectedAgent>();
@@ -206,7 +207,8 @@ public class PathogenManager : MonoBehaviour
                 kuramoto.phase = GPUStruct[i].phase;
                 kuramoto.Connections = GPUStruct[i].connections;
                 kuramoto.played = GPUStruct[i].played;
-                sentinels[i].GetComponent<Rigidbody>().AddForceAtPosition( GPUStruct[i].vel * speedScl, sentinels[i].transform.position + sentinels[i].transform.up);
+                float sinSpdScl = Mathf.Sin(Mathf.Deg2Rad * (kuramoto.phase * 360)) * speedScl;
+                sentinels[i].GetComponent<Rigidbody>().AddForceAtPosition( GPUStruct[i].vel * sinSpdScl, sentinels[i].transform.position - sentinels[i].transform.up);
                 
                 GPUStruct[i].pos = sentinels[i].transform.position;
 
